7e29818334205e28c0ec9d81715f84acc1490d33
Roberto <rscruz2017@gmail.com>
2025-06-26 14:34:17 -0300

    Exercicio mult

---
+++ Exercicio.py
@@ -0,0 +1,9 @@
+numero_1 = input("digite um numero:")
+numero_2 = input("digite um numero:")
+numero_3 = input("digite um numero:")
+numero_4 = input("digite um numero:")
+numero_5 = input("digite um numero:")
+
+resultado = int(numero_1)*int(numero_2)*int (numero_3)*int(numero_4)*int(numero_5)
+
+print(resultado)
